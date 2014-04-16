b5ad769065f58c6a6e5af5f5a296a3cc5fe6347e
Franco <f.raimondi@gmail.com>
2014-04-16 10:35:53 +0100

    cleaning up the house

--- main.rkt
+++ main.rkt
@@ -6,79 +6,103 @@
 
 #lang racket
 (require "MIRTOlib.rkt") ;Myrtle Library
-(require "linefollow.rkt") ;Line Following
 (require racket/system) ;Racket System Use For CLI
 (require web-server/servlet web-server/servlet-env) ;Web Server App
 
 (setup) ;Setup For Myrtle
 (enableIR) ;Enable IR Sensors On Myrtle
-(define lfollow null) ;Line Following Thread
 (define verify null) ;Controller Verification
 
-(define outfile (open-input-file "/tmp/sid.txt"))
 
 ;=================================================================================================================================================================
 (
  define (dweb x) ;Main function
   (define posts (request-bindings x)) ;Get Parameters
-  ( ;Server Link
-   if (and (exists-binding? 'link posts) (exists-binding? 'v posts)) ;If link command and GUID exists
+  
+  (cond 
+   
+   ;Server Link
+   ( (and (exists-binding? 'link posts) (exists-binding? 'v posts)) ;If link command and GUID exists
       (
        if(null? verify) ;If server verify is empty
          (and (set! verify (first (extract-bindings 'v posts))) (response/xexpr verify)) ;Set verify and response back
          (and (response/xexpr `(html (script "alert('This robot already linked!');")))) ;Response alert robot already linked
       )
-      ( ;Server Unlink
-       if (and (exists-binding? 'unlink posts) (exists-binding? 'v posts)) ;If unlink command and GUID exists
-          (
-           if(null? verify) ;If server verify is empty
-             (and (response/xexpr `(html (script "alert('This robot is empty!');")))) ;Response alert robot is empty
-             (
-              if (equal? verify (first (extract-bindings 'v posts))) ;If server verify = GUID
-                 (and (set! verify null) (response/xexpr "unlinked")) ;Set server verify to null and response back unlinked
-                 (and (response/xexpr `(html (script "alert('Verification does not match!');")))) ;Response back alert GUID does not match
+      ) ;; end of link
+      
+   ( ;Server Unlink
+    (and (exists-binding? 'unlink posts) (exists-binding? 'v posts)) ;If unlink command and GUID exists
+      (
+       if(null? verify) ;If server verify is empty
+         (and (response/xexpr `(html (script "alert('This robot is empty!');")))) ;Response alert robot is empty
+         (
+          if (equal? verify (first (extract-bindings 'v posts))) ;If server verify = GUID
+             (and (set! verify null) (response/xexpr "unlinked")) ;Set server verify to null and response back unlinked
+             (and (response/xexpr `(html (script "alert('Verification does not match!');")))) ;Response back alert GUID does not match
              )
-          )
-          ( ;Other Commands
-           if (and (exists-binding? 'v posts) (equal? verify (first (extract-bindings 'v posts)))) ;Check if verification exist and match
-              (
-               begin
-                ( ;Get IR Sensors
-                 if (exists-binding? 'getir posts)
-                    (response/xexpr `(div
-                                      (span ((style "margin-left:50px; color:#09F; background:black; font-size:30px; width:100px; height:100px;")) ,(number->string (getIR 3)))
-                                      (span ((style "margin-left:50px; color:#09F; background:black; font-size:30px; width:100px; height:100px;")) ,(number->string (getIR 2)))
-                                      (span ((style "margin-left:50px; color:#09F; background:black; font-size:30px; width:100px; height:100px;")) ,(number->string (getIR 1)))))
-                    ( ;Get Bumper
-                     if (exists-binding? 'getbump posts)
-                        (response/xexpr `(div
-                                          (img ((src ,(if (leftBump?) "img/LBumper_O.png" "img/LBumper.png")) (id "lb")))
-                                          (img ((src ,(if (rightBump?) "img/RBumper_O.png" "img/RBumper.png")) (id "rb")))
-                                          ))
+         )
+      ) ;; end of unlink
+      
+  ( ;Other Commands
+   (and (exists-binding? 'v posts) (equal? verify (first (extract-bindings 'v posts)))) ;Check if verification exist and match
+   
+   (cond 
+     ( ;Get IR Sensors
+      (exists-binding? 'getir posts)
+      (response/xexpr `(div
+                        (span ((style "margin-left:50px; color:#09F; background:black; font-size:30px; width:100px; height:100px;")) ,(number->string (getIR 3)))
+                        (span ((style "margin-left:50px; color:#09F; background:black; font-size:30px; width:100px; height:100px;")) ,(number->string (getIR 2)))
+                        (span ((style "margin-left:50px; color:#09F; background:black; font-size:30px; width:100px; height:100px;")) ,(number->string (getIR 1)))))
+      )
+     ( ;Get Bumper
+      (exists-binding? 'getbump posts)
+      (response/xexpr `(div
+                        (img ((src ,(if (leftBump?) "img/LBumper_O.png" "img/LBumper.png")) (id "lb")))
+                        (img ((src ,(if (rightBump?) "img/RBumper_O.png" "img/RBumper.png")) (id "rb")))
+                        ))
+      )
                         
-                        ( ;This is for movement
-                         if (and (exists-binding? 'move posts) (and (exists-binding? 'motor1 posts) (exists-binding? 'motor2 posts)))
-                            (and ;SetMotors and return string "Moved"
-                             (setMotors
-                              (string->number (first (extract-bindings 'motor1 posts)))
-                              (string->number (first (extract-bindings 'motor2 posts))))
-                             (response/xexpr "Moved"))
-                            ( ;This is for stop
-                             if (exists-binding? 'stop posts)
-                                (and (stopMotors) (response/xexpr "Stopped")) ;Stop motor and return string "Stopped"
-                                ( ;Action checker
-                                 if (and (exists-binding? 'act posts) (exists-binding? 'command posts) (equal? (first (extract-bindings 'command posts)) "capimg")) ;Toggle Image Capture
-                                    (
-                                     let ([imgname (number->string (current-seconds))]) ;Let image name be the current seconds
-                                      (and (system (string-append "streamer -f jpeg -s 640x480 -o /home/pi/dc/cap/" imgname ".jpeg")) ;System out capture image
-                                           (response/xexpr `(img ((src ,(string-append "/cap/" imgname ".jpeg"))))))
-                                      )
-                                    ( ;This is for line following
-                                     if (and (exists-binding? 'act posts) (exists-binding? 'command posts) (equal? (first (extract-bindings 'command posts)) "lfon")) ;Toggle Line Follow ON
-                                        (and (set! lfollow (thread (lambda () (controlLoop)))) (response/xexpr "Following Line"))
-                                        ( ;This is for stop line following
-                                         if (and (exists-binding? 'act posts) (exists-binding? 'command posts) (equal? (first (extract-bindings 'command posts)) "lfoff")) ;Toggle Line Follow OFF
-                                            (and (kill-thread lfollow) (stopMotors) (response/xexpr "Stop Following"))
+     ( ;This is for movement
+      (and (exists-binding? 'move posts) (and (exists-binding? 'motor1 posts) (exists-binding? 'motor2 posts)))
+      (and ;SetMotors and return string "Moved"
+       (setMotors
+        (string->number (first (extract-bindings 'motor1 posts)))
+        (string->number (first (extract-bindings 'motor2 posts))))
+       (response/xexpr "Moved"))
+      )
+
+     ( ;This is for stop
+      (exists-binding? 'stop posts)
+      (and (stopMotors) (response/xexpr "Stopped")) ;Stop motor and return string "Stopped"
+      )
+        
+     ( ;Action checker
+      (and (exists-binding? 'act posts) (exists-binding? 'command posts) (equal? (first (extract-bindings 'command posts)) "capimg")) ;Toggle Image Capture
+      (
+       let ([imgname (number->string (current-seconds))]) ;Let image name be the current seconds
+        (and (system (string-append "raspistill -t 500 -o /home/pi/dc/cap/" imgname ".jpeg")) ;System out capture image
+             (response/xexpr `(img ((src ,(string-append "/cap/" imgname ".jpeg"))))))
+        )
+      )
+        
+        ;; Add speech here
+        
+        
+        ) ;; end of cond verification exists and matches.
+   ) 
+  (else 
+      (response/xexpr "NONE")
+      )
+  
+  ) ;; end of big cond
+  ) ;; end of dweb
+               
+ ;;           ( ;This is for line following
+ ;;            if (and (exists-binding? 'act posts) (exists-binding? 'command posts) (equal? (first (extract-bindings 'command posts)) "lfon")) ;Toggle Line Follow ON
+ ;;               (and (set! lfollow (thread (lambda () (controlLoop)))) (response/xexpr "Following Line"))
+ ;;               ( ;This is for stop line following
+ ;;                if (and (exists-binding? 'act posts) (exists-binding? 'command posts) (equal? (first (extract-bindings 'command posts)) "lfoff")) ;Toggle Line Follow OFF
+ ;;                   (and (kill-thread lfollow) (stopMotors) (response/xexpr "Stop Following"))
                                             
                                             
                                             ;( ;This is for voice recognition
@@ -98,20 +122,20 @@
                                                     ; (response/xexpr "Talk Off")) ;Return string to client
                                                     ;(response/xexpr "NONE") ;This is when no command has been found above!!
                                                     ;);
-                                                )
-                                            )
-                                        )
-                                    )
-                                )
-                            )
-                        )
-                    )
-                )
-              (response/xexpr "NONE")
-              )
-      )  
-   )
- ;)
+;;                                                )
+;;                                            )
+;;                                        )
+;;                                    )
+;;                                )
+;;                            )
+;;                        )
+;;                    )
+;;                )
+;;              (response/xexpr "NONE")
+;;              )
+;;      )  
+;;   )
+;; )
 ;=================================================================================================================================================================
 
 ;Toggle Web Server
